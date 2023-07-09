59f8a43f6e3457c094438697ccffca6b3007035d
kirillye <kirill.stalker.2011@mail.ru>
2023-07-09 23:06:48 +0300

    finish 1.0.0

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,26 +1,21 @@
 const allowedCors = require("../util/params/allowCors");
 
 module.exports = (req, res, next) => {
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  const { origin } = req.headers;
+  const { method } = req;
   const requestHeaders = req.headers["access-control-request-headers"];
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
-  res.header("Access-Control-Allow-Credential", true);
+  res.header("Access-Control-Allow-Credentials", true);
 
   if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header("Access-Control-Allow-Origin", origin);
   }
 
   if (method === "OPTIONS") {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-
-    // разрешаем кросс-доменные запросы с этими заголовками
     res.header("Access-Control-Allow-Headers", requestHeaders);
 
-    // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
 
