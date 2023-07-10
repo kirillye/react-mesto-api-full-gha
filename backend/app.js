6c74418a0d6da33d4b37f640d5b559e8f8f6031a
kirillye <kirill.stalker.2011@mail.ru>
2023-07-10 23:58:18 +0300

    finish

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,11 @@ const { errors } = require("celebrate");
 const app = express();
 const routes = require("./routes");
 const mongoose = require("mongoose");
-// const cors = require("./middlewares/cors");
-const cors = require("cors");
+const cors = require("./middlewares/cors");
 
 app.use(cookieParser());
 app.use(express.json());
-// app.use(cors);
-app.use(cors());
+app.use(cors);
 
 mongoose
   .connect(`mongodb://127.0.0.1:27017/database`, {
